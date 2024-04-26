007b792e1bdf25f26c23b4efd5088aef1afee47e
KASHIF-SHOUKAT-SE <149269224+KASHIF-SHOUKAT-SE@users.noreply.github.com>
2024-04-26 13:22:59 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function oneplusavg(x,y){
+    return 1 + (a + b)/2
+}
+
+let a = 1;
+let b = 2;
+let c = 3;
+console.log("average of a and b is", oneplusavg(a,b))
+console.log("average of b and c is", oneplusavg(b,c))
+console.log("average of a and c is", oneplusavg(a,c))
